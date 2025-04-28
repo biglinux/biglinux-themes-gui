e971a1d624fc363c7bf35e3533e3ba0bf334f515
Bruno Gonçalves <bigbruno@gmail.com>
2025-04-27 21:10:42 -0300

    Simplify gettext import

--- usr/share/bigbashview/apps/biglinux-themes-gui/i18n.py
+++ usr/share/bigbashview/apps/biglinux-themes-gui/i18n.py
@@ -3,7 +3,5 @@ Internationalization module for BigLinux Themes GUI.
 Handles translation setup and provides the gettext function.
 """
 
-import gettext
-
-gettext.textdomain("biglinux-themes-gui")
-_ = gettext.gettext
+from gettext import translation
+_ = translation("biglinux-themes-gui").gettext
